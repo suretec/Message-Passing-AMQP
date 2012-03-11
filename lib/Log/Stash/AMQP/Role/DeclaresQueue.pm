a4b488524186c3aee419f421d472bfc04ffc1916
Tomas Doran <bobtfish@bobtfish.net>
2012-03-11 12:19:49 +0000

    Declare queue

---
+++ lib/Log/Stash/AMQP/Role/DeclaresQueue.pm
@@ -0,0 +1,53 @@
+package Log::Stash::AMQP::Role::DeclaresQueue;
+use Moose::Role;
+use Moose::Util::TypeConstraints;
+use namespace::autoclean;
+
+with 'Log::Stash::AMQP::Role::HasAChannel';
+
+has queue_name => (
+    is => 'ro',
+    isa => 'Str',
+    predicate => '_has_queue_name',
+    lazy => 1,
+    default => sub {
+        shift->_queue->queue;
+    }
+);
+
+# FIXME - Should auto-build from _queue as above
+has queue_type => (
+    is => 'ro',
+    isa => enum([qw/ topic direct fanout /]),
+    default => 'topic',
+);
+
+# FIXME - Should auto-build from _queue as above
+has queue_durable => (
+    is => 'ro',
+    isa => 'Bool',
+    default => 1,
+);
+
+has _queue => (
+    is => 'ro',
+    writer => '_set_queue',
+);
+
+after '_set_channel' => sub {
+    my $self = shift;
+    weaken($self);
+    $self->_channel->declare_queue(
+        durable => $self->queue_durable,
+        $self->_has_queue_name ? (queue => $self->queue_name) : (),
+        on_success => sub {
+            $self->_set_queue(shift());
+        },
+        on_failure => sub {
+            warn("Failed to get queue");
+            $self->_clear_channel;
+        },
+    );
+};
+
+1;
