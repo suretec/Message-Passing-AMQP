fa3c542143c7f1dd32b39b654b9bc929a4afdecb
Tomas Doran <bobtfish@bobtfish.net>
2012-03-11 11:45:51 +0000

    Start mocking stuff out

---
+++ lib/Log/Stash/AMQP/Role/DeclaresExchange.pm
@@ -0,0 +1,47 @@
+package Log::Stash::AMQP::Role::DeclaresExchange;
+use Moose::Role;
+use Moose::Util::TypeConstraints;
+use namespace::autoclean;
+
+with 'Log::Stash::AMQP::Role::HasAChannel';
+
+has exchange_name => (
+    is => 'ro',
+    required => 1,
+    isa => 'Str',
+);
+
+has exchange_type => (
+    is => 'ro',
+    isa => enum([qw/ topic direct fanout /]),
+    default => 'topic',
+);
+
+has exchange_durable => (
+    is => 'ro',
+    isa => 'Bool',
+    default => 1,
+);
+
+has _exchange => (
+    is => 'ro',
+    writer => '_set_exchange',
+);
+
+after _set_channel => sub {
+    my $self = shift;
+    weaken($self);
+    $self->_channel->declare_exchange(
+        type => $self->exchange_type,
+        durable => $self->exchange_durable,
+        exchange => $self->exchange_name,
+        on_success => sub {
+            $self->_set_exchange(shift()->method_frame);
+        },
+        on_failure => sub {
+            $self->_clear_channel;
+        },
+    );
+};
+
+1;
--- lib/Log/Stash/AMQP/Role/HasAChannel.pm
+++ lib/Log/Stash/AMQP/Role/HasAChannel.pm
@@ -1,6 +1,115 @@
 package Log::Stash::AMQP::Role::HasAChannel;
 use Moose::Role;
+use Scalar::Util qw/ weaken /;
+use AnyEvent;
+use AnyEvent::RabbitMQ;
 use namespace::autoclean;
+
+has hostname => (
+    is => 'ro',
+    isa => 'Str',
+    default => 'localhost',
+);
+
+has port => (
+    is => 'ro',
+    isa => 'Int',
+    default => 5672,
+);
+
+has [qw/ username password /] => (
+    is => 'ro',
+    isa => 'Str',
+    default => 'guest',
+);
+
+has vhost => (
+    is => 'ro',
+    isa => 'Str',
+    default => '/',
+);
+
+has timeout => (
+    is => 'ro',
+    isa => 'Int',
+    default => 10,
+);
+
+has _connecting_timer => (
+    is => 'ro',
+    clearer => '_clear_connecting_timer',
+    predicate => '_am_connecting',
+    reader => '_start_connecting',
+    lazy => 1,
+    default => sub {
+        my $self = shift;
+        weaken $self;
+        AnyEvent->timer(after => $self->timeout, cb => sub {
+            $self->_am_connected(0);
+            $self->_clear_connection;
+            $self->_clear_connecting_timer;
+            my $idle; $idle = AnyEvent->idle(cb => sub {
+                $self->_connection;
+                undef $idle;
+            });
+        });
+    },
+);
+
+has _channel => (
+    is => 'ro',
+    writer => '_set_channel',
+    clearer => '_clear_channel',
+);
+
+sub connected {
+    my $self = shift;
+    weaken($self);
+    $self->_connection->open_channel(
+        on_success => sub {
+            my $channel = shift;
+            $self->_set_channel($channel);
+        },
+        on_failure => sub {
+            $self->_clear_channel;
+        },
+        on_close => sub {
+            $self->_clear_channel;
+        },
+    );
+}
+sub disconnected {}
+
+has _connection => (
+    is => 'ro',
+    lazy => 1,
+    default => sub {
+        my $self = shift;
+        #weaken($self);
+        $self->_connecting_timer;
+        AnyEvent::RabbitMQ->new->load_xml_spec()->connect(
+            host       => $self->hostname,
+            port       => $self->port,
+            user       => $self->username,
+            pass       => $self->password,
+            vhost      => $self->vhost,
+            timeout    => $self->timout,
+            on_success => sub {
+                $self->connected();
+                $self->_clear_connecting_timer;
+            },
+            on_failure => sub {
+                $self->disconnected();
+                $self->_clear_connection;
+            },
+            on_close => sub {
+                $self->disconnected();
+                $self->_clear_connection;
+            },
+        );
+    },
+    clearer => '_clear_connection',
+);
 
 1;
 
